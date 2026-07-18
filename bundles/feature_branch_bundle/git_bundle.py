11fa3cef137f47c2b9617bb5a6821544fbd614ea
zcx5674 <zcx5674@gmail.com>
2026-07-18 20:40:42 +0900

    Add test case

--- bundles/feature_branch_bundle/git_bundle.py
+++ bundles/feature_branch_bundle/git_bundle.py
@@ -150,7 +150,7 @@ class FeatureBranchGitDagBundle(BaseDagBundle):
                 shutil.rmtree(self.bundle_path)
             self.bundle_path.mkdir(parents=True, exist_ok=True)
 
-            import time; time.sleep(20) # for test
+            # import time; time.sleep(20) # for test
 
             # Fetch all branches
             cm = self.hook.configure_hook_env() if self.hook else nullcontext()
