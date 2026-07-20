b92a69afbf83acabe696f547ffda239b81fd1239
zcx5674 <zcx5674@gmail.com>
2026-07-20 16:48:18 +0900

    Add test case

--- dags/metrics_test2/dag_metrics_test2.py
+++ dags/metrics_test2/dag_metrics_test2.py
@@ -4,6 +4,13 @@ from airflow.sdk import dag, task
 from pendulum import datetime
 import time
 
+# --- TEMP DEBUG: dump parse-time sys.path so we can see what's importable ---
+import sys as _dbg_sys, os as _dbg_os
+print("PARSE_SYSPATH:", _dbg_sys.path, flush=True)
+print("PARSE_CWD:", _dbg_os.getcwd(), flush=True)
+print("PARSE_FILE:", __file__, flush=True)
+# --- END TEMP DEBUG ---
+
 
 @dag(
     start_date=datetime(2025, 1, 1),
